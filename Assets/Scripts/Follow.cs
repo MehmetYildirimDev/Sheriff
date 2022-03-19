14d4983f9cecaecd1ae88658e55d0a0f6277078c
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-03-19 19:50:57 +0300

    rafactor: follow script

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,20 +6,52 @@ using UnityEngine.AI;
 public class Follow : MonoBehaviour
 {
     private NavMeshAgent agent;
-    [SerializeField] private GameObject go;
+
+
+    [SerializeField] private GameObject Mine;
+    [SerializeField] private GameObject hero;
+    [SerializeField] private GameObject runner;
+    [SerializeField] private GameObject goldstatic;
+    private GameObject emtyp;
+
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+
+    }
+    private void Start()
+    {
+        emtyp = Mine.transform.GetChild(0).gameObject;
     }
 
     private void Update()
     {
-        agent.destination = go.transform.position;
-
-        if (gameObject.transform.position==go.transform.position)
-        {
-            Debug.Log("ayný konum");
-        }
+            agent.destination = emtyp.transform.position;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gold"))
+        {
+            emtyp = runner;
+            Destroy(other.gameObject, 1f);
+        }
+        if (other.gameObject.CompareTag("runner"))
+        {
+            goldstatic.gameObject.transform.localScale = 1.2f * goldstatic.gameObject.transform.localScale;
+
+
+            if (Mine.transform.childCount>0)
+            {
+                emtyp = Mine.transform.GetChild(0).gameObject;
+            }
+            else
+            {
+                Debug.Log("Gold kalmadý");
+                Time.timeScale = 0;
+            }
+            
+        }
+    }
 }
