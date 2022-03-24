b715272f18e7575cb2e69d86af1c2ebdbd99179b
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-03-24 19:00:49 +0300

    feat: add DayLoop

---
+++ Assets/DayLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayLoop : MonoBehaviour
+{
+    public Material[] skyboxMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.right, 2f * Time.deltaTime);
+
+        //bir nokta etrafýnda döndürmek için kullanýlýr bu fonksiyon
+        //noktayý olusturduk ilk parametrede sonra yönu sectik , zamaný seçtik 
+    }
+}
