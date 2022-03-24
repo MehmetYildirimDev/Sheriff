0cf8c62287455c43683a04f9a3cf21da2f493cd4
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-03-25 01:17:36 +0300

    feat : create ammo system

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunScript : MonoBehaviour
 {
@@ -16,26 +17,78 @@ public class GunScript : MonoBehaviour
 
     public Camera FpsCamera;
 
-    public ParticleSystem muzzleFLash;
+    public ParticleSystem muzzleFlash;
 
     public GameObject impactEffect;
     public float impactForce = 30f;
 
     private float nextTimetoFire = 0f;
+
+    private AudioSource audioSource;
+    public AudioClip[] audios;
+
+
+    public int Totalbullet = 90;
+    public int clipbullet = 30;
+    public int M4BULLET = 30;
+    public int empty = 0;
+    public float ReloadTime = 2;
+    public Text clip;
+    public Text TotalAmmo;
+    private bool canShoot = true;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
+        clip.text = clipbullet.ToString();
+        TotalAmmo.text = Totalbullet.ToString();
+    }
+
     void Update()
     {
-
-
-        if (Input.GetButton("Fire1") && Time.time >= nextTimetoFire)
+        if (Totalbullet < 0)
         {
-            nextTimetoFire = Time.time + 1f / fireRate;
-            Shoot();
+            canShoot = false;
         }
+
+        clip.text = clipbullet + "/";
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimetoFire && canShoot)
+        {
+
+
+            nextTimetoFire = Time.time + 1f / fireRate;
+            if (clipbullet > 0)
+            {
+                Shoot();
+            }
+            else
+            {
+                ReloadClip();
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadClip();
+        }
+
     }
 
     void Shoot()
     {
-        muzzleFLash.Play();
+        //        Totalbullet -= 1;
+        clipbullet -= 1;
+
+
+        muzzleFlash.Play();
+
+        audioSource.PlayOneShot(audios[0]);
 
         RaycastHit hit;
 
@@ -47,11 +100,11 @@ public class GunScript : MonoBehaviour
             //            Damage Target = hit.transform.GetComponent<Damage>();
 
 
-           EnemyManager enemy = hit.transform.GetComponent<EnemyManager>();
-           if (enemy != null)
-           {
-               enemy.DamageTake(damage);
-           }
+            EnemyManager enemy = hit.transform.GetComponent<EnemyManager>();
+            if (enemy != null)
+            {
+                enemy.DamageTake(damage);
+            }
 
 
 
@@ -63,6 +116,27 @@ public class GunScript : MonoBehaviour
             GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGO, 1f);
         }
+    }
+
+    void ReloadClip()
+    {
+        //25 mermi var ve totalde 10 mermi kaaldý 
+        if (Totalbullet > 0)
+        {
+            empty = M4BULLET - clipbullet;
+
+            if (empty > Totalbullet)
+            {
+                empty = Totalbullet;
+            }
+            Totalbullet -= empty;
+            clipbullet += empty;
+        }
+
+
+
+
+        TotalAmmo.text = Totalbullet.ToString();
     }
 
 
